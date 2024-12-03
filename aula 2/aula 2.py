596b51888c1eca5675f444e5290acd92b4e5c168
sabrinalanysilva <sabrinalany8@gmail.com>
2024-12-03 11:37:35 -0300

    Add files via upload

---
+++ aula 2/aula 2.py
@@ -0,0 +1,11 @@
+print('seja bem vindo')
+
+nome_usuario = 'sabrina lany' # tipo - str =strir
+idade_usuario = 15 #numero
+altura_usuario = 1.66 # tipo - float =ponto flutante
+maior_idade = False # tipo - bool = boolean
+
+print('Seja bem-vindo', nome_usuario)
+print('Sua idade é', idade_usuario, 'anos')
+print('Voce tem', altura_usuario, 'metros.')
+print('Voce é maior de idade?', maior_idade)
